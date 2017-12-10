19483f80f87b72c71fc4b77e6dabc03c6e779fb6
Andrew Radev <andrey.radev@gmail.com>
2017-12-10 12:03:28 +0200

    Get random word from wordlist

--- src/wordlist.rs
+++ src/wordlist.rs
@@ -1,4 +1,7 @@
+extern crate rand;
+
 use std::io::BufRead;
+use self::rand::Rng;
 
 pub struct Wordlist {
     words: Vec<String>,
@@ -10,12 +13,25 @@ impl Wordlist {
             lines().
             filter_map(Result::ok).
             map(|l| l.trim().to_string()).
-            collect();
+            filter(|l| l.chars().all(char::is_alphabetic)).
+            filter(|l| l.len() > 0).
+            collect::<Vec<String>>();
+
+        if words.len() == 0 {
+            panic!("Attempted to initialize empty wordlist!");
+        }
 
         Wordlist { words }
     }
 
     pub fn get(&self, n: usize) -> Option<&String> {
         self.words.get(n)
+    }
+
+    pub fn random(&self) -> &String {
+        let mut rng = rand::thread_rng();
+        let n = rng.gen_range(0, self.words.len());
+
+        &self.words[n]
     }
 }
--- tests/test_wordlist.rs
+++ tests/test_wordlist.rs
@@ -2,7 +2,7 @@ extern crate hangman;
 use hangman::wordlist::Wordlist;
 
 #[test]
-fn test_something() {
+fn test_initialize_wordlist() {
     let data: &[u8] = br"
         one
         two
@@ -10,5 +10,35 @@ fn test_something() {
     ";
     let wordlist = Wordlist::from_io(data);
 
-    assert_eq!(wordlist.get(2), Some(&String::from("two")));
+    assert_eq!(wordlist.get(1), Some(&String::from("two")));
+}
+
+#[test]
+fn test_wordlist_ignores_invalid_words() {
+    let data: &[u8] = br"
+        _foo_
+        one
+        123
+    ";
+    let wordlist = Wordlist::from_io(data);
+
+    assert_eq!(wordlist.get(0), Some(&String::from("one")));
+    assert_eq!(wordlist.get(1), None);
+}
+
+#[test]
+#[should_panic]
+fn test_invalid_wordlist_empty() {
+    let data: &[u8] = br"";
+    Wordlist::from_io(data);
+}
+
+#[test]
+fn test_random_word() {
+    let data: &[u8] = br"
+        one
+    ";
+    let wordlist = Wordlist::from_io(data);
+
+    assert_eq!(wordlist.random(), &String::from("one"));
 }
